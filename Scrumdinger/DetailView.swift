a5eda64f99df144f28a9fdcd58884afbbaf0476e
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-09-29 23:35:42 +0300

    Update DetailView

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct DetailView: View {
+    let  scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Lenght", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
     }
 }
 
 #Preview {
-    DetailView()
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
 }
