81bb76902d81092e17fd7e1b1e5786377c9749bb
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-09-29 23:22:00 +0300

    Added Navigation

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct CardView: View {
+    let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .padding(.leading, 20)
+                .padding(.top, 5)
+                .accessibilityAddTraits(.isHeader)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", 
+                      systemImage: "person.3")
+                .accessibilityLabel("\(scrum.attendees.count) attendees")
+                .padding(.leading, 20)
+                .padding(.bottom, 5)
+                Spacer()
+                Label("\(scrum.lengthInMinutes)",
+                      systemImage: "clock")
+                .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+
+                .labelStyle(.trailingIcon)
+                .padding(.trailing, 20)
+                .padding(.bottom, 5)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
-#Preview {
-    CardView()
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
 }
+
+
--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,18 @@
 //  Created by HALUK BAYRAKCI on 29.09.2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
