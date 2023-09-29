0b09de6b59deba1165f710b0ac3a5c2f3d42d2fa
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-09-29 22:36:29 +0300

    Added scrums identifiable

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by HALUK BAYRAKCI on 29.09.2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
