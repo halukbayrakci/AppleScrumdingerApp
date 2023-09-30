911def87cea84a75e116c85b46ae1e778fc0acdc
HALUK BAYRAKCI <halukbayrakci32@gmail.com>
2023-09-30 14:23:39 +0300

    Added theme picker

---
+++ Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,25 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by HALUK BAYRAKCI on 30.09.2023.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+#Preview {
+    ThemePicker(selection: .constant(.periwinkle))
+}
